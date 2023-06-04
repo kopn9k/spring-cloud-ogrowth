c5fe0c4f0c429fd74f9e08eacd4d1891533955dc
kopn9k <pashaorashkov@mail.ru>
2023-06-05 00:56:03 +0300

    Initialize licence-service

---
+++ license-service/src/main/java/com/pasha/licenseservice/LicenseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pasha.licenseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseServiceApplication.class, args);
+	}
+
+}
