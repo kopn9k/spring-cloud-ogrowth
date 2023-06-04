14af7493cb13c3e22d2ddc39f19d7dd7844207fc
kopn9k <pashaorashkov@mail.ru>
2023-06-04 15:58:54 +0300

    Initial commit

---
+++ src/main/java/com/pasha/springcloudogrowth/SpringCloudOgrowthApplication.java
@@ -0,0 +1,13 @@
+package com.pasha.springcloudogrowth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOgrowthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudOgrowthApplication.class, args);
+    }
+
+}
