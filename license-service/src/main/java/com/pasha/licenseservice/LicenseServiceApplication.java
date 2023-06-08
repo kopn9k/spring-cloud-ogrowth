079a547a3163e8d4a7e6e25b9443d2d5a8c9d23a
kopn9k <pashaorashkov@mail.ru>
2023-06-08 18:28:58 +0300

    Register license service with eureka discovery client

--- license-service/src/main/java/com/pasha/licenseservice/LicenseServiceApplication.java
+++ license-service/src/main/java/com/pasha/licenseservice/LicenseServiceApplication.java
@@ -2,6 +2,7 @@ package com.pasha.licenseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -12,6 +13,7 @@ import java.util.Locale;
 
 @SpringBootApplication
 @RefreshScope //currently manual, by calling /actuator/refresh, later cloud bus need be added
+@EnableDiscoveryClient
 public class LicenseServiceApplication {
 
 	public static void main(String[] args) {
