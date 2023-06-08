e113ebc75545459d952db855c4f3ca071b5a2a10
kopn9k <pashaorashkov@mail.ru>
2023-06-09 02:43:04 +0300

    Refactoring

--- license-service/src/main/java/com/pasha/licenseservice/service/client/OrganizationFeignClient.java
+++ license-service/src/main/java/com/pasha/licenseservice/service/client/OrganizationFeignClient.java
@@ -2,10 +2,8 @@ package com.pasha.licenseservice.service.client;
 
 import com.pasha.licenseservice.model.Organization;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(value = "organization-service")
 public interface OrganizationFeignClient {
--- organization-service/src/main/java/com/pasha/organizationservice/OrganizationServiceApplication.java
+++ organization-service/src/main/java/com/pasha/organizationservice/OrganizationServiceApplication.java
@@ -2,7 +2,6 @@ package com.pasha.organizationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
