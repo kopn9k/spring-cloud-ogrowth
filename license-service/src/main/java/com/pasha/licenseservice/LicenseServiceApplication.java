9fc33f1ca3fbe25770b12c618c775e30cd525379
kopn9k <pashaorashkov@mail.ru>
2023-06-09 02:29:33 +0300

    Add OrganizationRestTemplateClient realization

--- license-service/src/main/java/com/pasha/licenseservice/LicenseServiceApplication.java
+++ license-service/src/main/java/com/pasha/licenseservice/LicenseServiceApplication.java
@@ -3,9 +3,11 @@ package com.pasha.licenseservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -33,6 +35,12 @@ public class LicenseServiceApplication {
 		messageSource.setUseCodeAsDefaultMessage(true);
 		messageSource.setBasenames("messages");
 		return messageSource;
+	}
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
--- license-service/src/main/java/com/pasha/licenseservice/service/client/OrganizationRestTemplateClient.java
+++ license-service/src/main/java/com/pasha/licenseservice/service/client/OrganizationRestTemplateClient.java
@@ -1,11 +1,26 @@
 package com.pasha.licenseservice.service.client;
 
 import com.pasha.licenseservice.model.Organization;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class OrganizationRestTemplateClient {
+
+    private final RestTemplate restTemplate;
+
+    public OrganizationRestTemplateClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     public Organization getOrganization(String organizationId) {
-        return new Organization("default", "default", "default", "default", "default");
+        ResponseEntity<Organization> response = restTemplate.exchange(
+                "http://organization-service/api/v1/organization/{organizationId}",
+                HttpMethod.GET,
+                null, Organization.class, organizationId);
+
+        return  response.getBody();
     }
 }
