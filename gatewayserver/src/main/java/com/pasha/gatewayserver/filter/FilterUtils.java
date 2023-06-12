55e0a41ae6a608f04c4ded30b8c8ae17b62992b7
kopn9k <pashaorashkov@mail.ru>
2023-06-12 13:12:09 +0300

    Add filter to set correlation id for all requests

---
+++ gatewayserver/src/main/java/com/pasha/gatewayserver/filter/FilterUtils.java
@@ -0,0 +1,41 @@
+package com.pasha.gatewayserver.filter;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class FilterUtils {
+    public static final String CORRELATION_ID = "tmx-correlation-id";
+    public static final String AUTH_TOKEN = "tmx-auth-token";
+    public static final String USER_ID = "tmx-user-id";
+    public static final String ORG_ID = "tmx-org-id";
+    public static final String PRE_FILTER_TYPE = "pre";
+    public static final String POST_FILTER_TYPE = "post";
+    public static final String ROUTE_FILTER_TYPE = "route";
+
+    public String getCorrelationId(HttpHeaders httpHeaders) {
+        if (httpHeaders.get(CORRELATION_ID) != null) {
+            List<String> header = httpHeaders.get(CORRELATION_ID);
+            return header.stream().findFirst().orElse(null);
+        } else {
+            return null;
+        }
+    }
+
+    public ServerWebExchange setCorrelationId(ServerWebExchange exchange, String correlationId) {
+        return setRequestHeader(exchange, CORRELATION_ID, correlationId);
+    }
+
+    private ServerWebExchange setRequestHeader(ServerWebExchange exchange, String name, String value) {
+        return exchange.mutate().request(
+                exchange.getRequest().mutate()
+                        .header(name, value)
+                        .build())
+                .build();
+    }
+
+}
---
+++ gatewayserver/src/main/java/com/pasha/gatewayserver/filter/TrackingFilter.java
@@ -0,0 +1,49 @@
+package com.pasha.gatewayserver.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Component
+@Order(1)
+public class TrackingFilter implements GlobalFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(TrackingFilter.class);
+
+    private final FilterUtils filterUtils;
+
+    public TrackingFilter(FilterUtils filterUtils) {
+        this.filterUtils = filterUtils;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        HttpHeaders httpHeaders = exchange.getRequest().getHeaders();
+        if (isCorrelationIdPresent(httpHeaders)) {
+            logger.debug("tmx-correlation-id found in tracking filter: {}. ",
+                    filterUtils.getCorrelationId(httpHeaders));
+        } else {
+            String correlationId = generateCorrelationId();
+            exchange = filterUtils.setCorrelationId(exchange, correlationId);
+            logger.debug("tmx-correlation-id generated in tracking filter: {}.", correlationId);
+        }
+
+        return chain.filter(exchange);
+    }
+
+    private boolean isCorrelationIdPresent(HttpHeaders httpHeaders) {
+        return filterUtils.getCorrelationId(httpHeaders) != null;
+    }
+
+
+    private String generateCorrelationId() {
+        return java.util.UUID.randomUUID().toString();
+    }
+}
