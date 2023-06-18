ca69a6157aada34a51ef73453ce569c3bbea2b8f
kopn9k <pashaorashkov@mail.ru>
2023-06-19 02:23:27 +0300

    Add micrometer tracing id to the response headers of gateway server

--- gatewayserver/src/main/java/com/pasha/gatewayserver/filter/ResponseFilter.java
+++ gatewayserver/src/main/java/com/pasha/gatewayserver/filter/ResponseFilter.java
@@ -1,33 +1,43 @@
 package com.pasha.gatewayserver.filter;
 
+import io.micrometer.tracing.Tracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@Component
-public class ResponseFilter implements GlobalFilter {
+@Configuration
+public class ResponseFilter {
 
     final Logger logger = LoggerFactory.getLogger(ResponseFilter.class);
 
     private final FilterUtils filterUtils;
 
-    public ResponseFilter(FilterUtils filterUtils) {
+    private final Tracer tracer;
+
+    public ResponseFilter(FilterUtils filterUtils, Tracer tracer) {
         this.filterUtils = filterUtils;
+        this.tracer = tracer;
     }
 
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            HttpHeaders httpHeaders = exchange.getRequest().getHeaders();
-            String correlationId = filterUtils.getCorrelationId(httpHeaders);
-            logger.debug("Adding the correlation id to the outbound headers. {}", correlationId);
-            exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
-            logger.debug("Completing outgoing request for {}.", exchange.getRequest().getURI());
+
+    @Bean
+    public GlobalFilter postGlobalfilter() {
+        return (exchange, chain) -> chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            if (tracer.currentSpan() != null && tracer.currentSpan().context() != null) {
+                String traceId = tracer.currentSpan().context().traceId();
+                logger.debug("Adding the correlation id to the outbound headers. {}", traceId);
+                exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, traceId);
+                logger.debug("Completing outgoing request for {}.", exchange.getRequest().getURI());
+            } else {
+                logger.debug("CurrentSpan or it's context is null. CurrentSpan - {}", tracer.currentSpan());
+            }
         }));
     }
 }
